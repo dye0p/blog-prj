4fba8a64ece2e912ff98ee3093974ae48d2e71f5
DongYeop <s01054830455@gmail.com>
2024-03-06 14:55:48 +0900

    회원 엔티티

---
+++ src/main/java/com/dyeop/blog/damain/User.java
@@ -0,0 +1,22 @@
+package com.dyeop.blog.damain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class User extends BaseEntity{ //회원 엔티티
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+
+}
